638db08fd93fdcb2262c45a660747fb7d06f7048
Ana <anali127@hotmail.com>
2022-05-14 17:34:37 -0400

    County correction

--- # Repetition Statements.py
+++ # Repetition Statements.py
@@ -7,5 +7,9 @@ while x <= 5:
 numbers = [0, 1, 2, 3, 4]
 for num in numbers:
     print(num)
-    
+
+#115 
+if (vote_count > winning_count) and (
+            vote_count_percentage > winning_county_percentage
+        ):
 
